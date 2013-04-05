756f8a851483fd7d9e0e3e4fa3bafb5833e1e23b
Alexey Ragozin <alexey.ragozin@gmail.com>
2013-04-05 14:28:56 +0000

    [jvm-attach-api] Dead object histogram utility
    git-svn-id: https://gridkit.googlecode.com/svn/grid-lab/trunk/nimble/jvm-attach-api@2921 4e96dbfe-9890-11de-ac5d-73b9d092dc0a

--- src/main/java/org/gridkit/lab/jvm/attach/HeapHisto.java
+++ src/main/java/org/gridkit/lab/jvm/attach/HeapHisto.java
@@ -7,8 +7,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.management.RuntimeErrorException;
-
 public class HeapHisto {
 
 	public static HeapHisto getHistoDead(int pid, long timeoutMs) {
