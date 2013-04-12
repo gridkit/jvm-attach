721f86c545088fe2c4b711c26343610c1d123725
Alexey Ragozin <alexey.ragozin@gmail.com>
2013-04-12 14:08:23 +0000

    [jvm-attach-api] Method to call printflag command on attached JVM
    git-svn-id: https://gridkit.googlecode.com/svn/grid-lab/trunk/systools/jvm-attach-api@2934 4e96dbfe-9890-11de-ac5d-73b9d092dc0a

--- src/main/java/org/gridkit/lab/jvm/attach/AttachManager.java
+++ src/main/java/org/gridkit/lab/jvm/attach/AttachManager.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
